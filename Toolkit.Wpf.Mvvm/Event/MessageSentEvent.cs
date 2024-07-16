9fe34ba550b7347cd05a4b5e25210d71cbb08232
Pohan <oange6214@gmail.com>
2024-07-16 22:42:06 +0800

    Add Event Aggregator functionality - Implement Event Aggregator

---
+++ Toolkit.Wpf.Mvvm/Event/MessageSentEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace Toolkit.Wpf.Mvvm.Event;
+
+public class MessageSentEvent
+{
+    public string Message { get; }
+
+    public MessageSentEvent(string message)
+    {
+        Message = message;
+    }
+}
